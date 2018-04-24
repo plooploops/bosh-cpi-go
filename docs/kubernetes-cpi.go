b2a1417d25e816d9a013fcbba996bb86af39544f
Andy <andy.gee@microsoft.com>
2018-04-24 16:51:02 -0700

    WIP testing k8s

---
+++ docs/kubernetes-cpi.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"os"
+
+	"flag"
+	"fmt"
+	"path/filepath"
+	"time"
+
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	"github.com/cppforlife/bosh-cpi-go/apiv1"
+	"github.com/cppforlife/bosh-cpi-go/rpc"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type CPIFactory struct{}
+
+type CPI struct{}
+
+var _ apiv1.CPIFactory = CPIFactory{}
+var _ apiv1.CPI = CPI{}
+
+func main() {
+	testClient()
+	logger := boshlog.NewLogger(boshlog.LevelNone)
+
+	cli := rpc.NewFactory(logger).NewCLI(CPIFactory{})
+
+	err := cli.ServeOnce()
+	if err != nil {
+		logger.Error("main", "Serving once: %s", err)
+		os.Exit(1)
+	}
+}
+
+func testClient() {
+	var kubeconfig *string
+
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, "source", "repos", "bosh-cpi-go", "docs", "config"), "(optional) absolute path to the kubeconfig file")
+
+		// kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+		// Examples for error handling:
+		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		namespace := "default"
+		pod := "example-xxxxx"
+		_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+		}
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
+// Empty CPI implementation
+
+func (f CPIFactory) New(_ apiv1.CallContext) (apiv1.CPI, error) {
+	return CPI{}, nil
+}
+
+func (c CPI) Info() (apiv1.Info, error) {
+	return apiv1.Info{}, nil
+}
+
+func (c CPI) CreateStemcell(imagePath string, _ apiv1.StemcellCloudProps) (apiv1.StemcellCID, error) {
+	return apiv1.NewStemcellCID("stemcell-cid"), nil
+}
+
+func (c CPI) DeleteStemcell(cid apiv1.StemcellCID) error {
+	return nil
+}
+
+func (c CPI) CreateVM(
+	agentID apiv1.AgentID, stemcellCID apiv1.StemcellCID,
+	cloudProps apiv1.VMCloudProps, networks apiv1.Networks,
+	associatedDiskCIDs []apiv1.DiskCID, env apiv1.VMEnv) (apiv1.VMCID, error) {
+
+	return apiv1.NewVMCID("vm-cid"), nil
+}
+
+func (c CPI) DeleteVM(cid apiv1.VMCID) error {
+	return nil
+}
+
+func (c CPI) CalculateVMCloudProperties(res apiv1.VMResources) (apiv1.VMCloudProps, error) {
+	return apiv1.NewVMCloudPropsFromMap(map[string]interface{}{}), nil
+}
+
+func (c CPI) SetVMMetadata(cid apiv1.VMCID, metadata apiv1.VMMeta) error {
+	return nil
+}
+
+func (c CPI) HasVM(cid apiv1.VMCID) (bool, error) {
+	return false, nil
+}
+
+func (c CPI) RebootVM(cid apiv1.VMCID) error {
+	return nil
+}
+
+func (c CPI) GetDisks(cid apiv1.VMCID) ([]apiv1.DiskCID, error) {
+	return []apiv1.DiskCID{}, nil
+}
+
+func (c CPI) CreateDisk(size int,
+	cloudProps apiv1.DiskCloudProps, associatedVMCID *apiv1.VMCID) (apiv1.DiskCID, error) {
+
+	return apiv1.NewDiskCID("disk-cid"), nil
+}
+
+func (c CPI) DeleteDisk(cid apiv1.DiskCID) error {
+	return nil
+}
+
+func (c CPI) AttachDisk(vmCID apiv1.VMCID, diskCID apiv1.DiskCID) error {
+	return nil
+}
+
+func (c CPI) DetachDisk(vmCID apiv1.VMCID, diskCID apiv1.DiskCID) error {
+	return nil
+}
+
+func (c CPI) HasDisk(cid apiv1.DiskCID) (bool, error) {
+	return false, nil
+}

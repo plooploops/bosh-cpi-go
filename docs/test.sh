03f5c7d9937b27fc4ac052cab33654d21193e9ba
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-04-26 16:59:07 -0700

    Add test script

--- docs/test.sh
+++ docs/test.sh
@@ -1,6 +1,25 @@
-#!/bin/bah
+#!/bin/bash
 
-stemcell_cid = cat create_stemcell | ./cpi | jq -r ".result"
-vm_cid = cat create_vm | ./cpi | jq -r ".result"
-disk_cid = cat create_disk | ./cpi | jq -r ".result"
-cat attach_disk | ./cpi
+executable=./cpi
+
+stemcell_cid=$(cat ./create_stemcell | \
+   ${executable} | \
+   jq -r ".result")
+echo "Stemcell is ${stemcell_cid}"
+
+vm_cid=$(cat ./create_vm | \
+   jq ".arguments[1] = \"${stemcell_cid}\"" | \
+   ${executable} | \
+   jq -r ".result")
+echo "VM is ${vm_cid}"
+
+disk_cid=$(cat ./create_disk | \
+   ${executable} | \
+   jq -r ".result")
+echo "Disk is ${disk_cid}"
+
+attach_result=$(cat ./attach_disk | \
+   jq ".arguments[0]=\"${vm_cid}\"" | \
+   jq ".arguments[1]=\"${disk_cid}\"" | \
+   ${executable})
+echo "Attaching disk is ${attach_result}"

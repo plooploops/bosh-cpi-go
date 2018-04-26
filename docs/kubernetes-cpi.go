9606f68a5046050a9b93879bf94f0aae7d179577
Jason Stevens <jason.stevens@microsoft.com>
2018-04-26 16:40:01 -0700

    Stemcell

--- docs/kubernetes-cpi.go
+++ docs/kubernetes-cpi.go
@@ -89,7 +89,8 @@ func (c CPI) Info() (apiv1.Info, error) {
 }
 
 func (c CPI) CreateStemcell(imagePath string, _ apiv1.StemcellCloudProps) (apiv1.StemcellCID, error) {
-	return apiv1.NewStemcellCID("stemcell-cid"), nil
+        // Handwave and always use the base ubuntu
+	return apiv1.NewStemcellCID("ubuntu:14.04"), nil
 }
 
 func (c CPI) DeleteStemcell(cid apiv1.StemcellCID) error {
@@ -114,7 +115,7 @@ func (c CPI) CreateVM(
 			Containers: []corev1.Container{
 				{
 					Name:  vmcid,
-					Image: "jupyter/minimal-notebook",
+					Image: stemcellCID.AsString(),
 					Ports: []corev1.ContainerPort{
 						{
 							ContainerPort: 8888,

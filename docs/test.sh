620637b524cb1799c3a3ddc3455200513bbc59ad
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2018-04-26 17:29:26 -0700

    Moved template files

--- docs/test.sh
+++ docs/test.sh
@@ -2,23 +2,23 @@
 
 executable=./cpi
 
-stemcell_cid=$(cat ./create_stemcell | \
+stemcell_cid=$(cat ./templates/create_stemcell.json | \
    ${executable} | \
    jq -r ".result")
 echo "Stemcell is ${stemcell_cid}"
 
-vm_cid=$(cat ./create_vm | \
+vm_cid=$(cat ./templates/create_vm.json | \
    jq ".arguments[1] = \"${stemcell_cid}\"" | \
    ${executable} | \
    jq -r ".result")
 echo "VM is ${vm_cid}"
 
-disk_cid=$(cat ./create_disk | \
+disk_cid=$(cat ./templates/create_disk.json | \
    ${executable} | \
    jq -r ".result")
 echo "Disk is ${disk_cid}"
 
-attach_result=$(cat ./attach_disk | \
+attach_result=$(cat ./templates/attach_disk.json | \
    jq ".arguments[0]=\"${vm_cid}\"" | \
    jq ".arguments[1]=\"${disk_cid}\"" | \
    ${executable})
